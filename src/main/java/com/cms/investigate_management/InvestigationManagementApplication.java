12128ee69cb1e65ca1316bf4cf613c9140ed8202
Ashwini <gowdaash30@gmail.com>
2026-07-24 19:58:42 +0530

    Create InvestigationManagementApplication.java

---
+++ src/main/java/com/cms/investigate_management/InvestigationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cms.investigate_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestigateManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
